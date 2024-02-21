329ebbf42ff3773230ab76fd65e95c5f06a8a271
vplasencia <v.pcalana@gmail.com>
2024-02-21 17:33:41 +0100

    feat(semaphorev4): add prettier

--- semaphorev4/hardhat.config.ts
+++ semaphorev4/hardhat.config.ts
@@ -1,9 +1,9 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config"
 
-import "@matterlabs/hardhat-zksync-node";
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
-import "@matterlabs/hardhat-zksync-verify";
+import "@matterlabs/hardhat-zksync-node"
+import "@matterlabs/hardhat-zksync-deploy"
+import "@matterlabs/hardhat-zksync-solc"
+import "@matterlabs/hardhat-zksync-verify"
 
 const config: HardhatUserConfig = {
   defaultNetwork: "zkSyncSepoliaTestnet",
@@ -12,44 +12,47 @@ const config: HardhatUserConfig = {
       url: "https://sepolia.era.zksync.dev",
       ethNetwork: "sepolia",
       zksync: true,
-      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification"
     },
     zkSyncMainnet: {
       url: "https://mainnet.era.zksync.io",
       ethNetwork: "mainnet",
       zksync: true,
-      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+      verifyURL:
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification"
     },
-    zkSyncGoerliTestnet: { // deprecated network
+    zkSyncGoerliTestnet: {
+      // deprecated network
       url: "https://testnet.era.zksync.dev",
       ethNetwork: "goerli",
       zksync: true,
-      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      verifyURL:
+        "https://zksync2-testnet-explorer.zksync.dev/contract_verification"
     },
     dockerizedNode: {
       url: "http://localhost:3050",
       ethNetwork: "http://localhost:8545",
-      zksync: true,
+      zksync: true
     },
     inMemoryNode: {
       url: "http://127.0.0.1:8011",
       ethNetwork: "localhost", // in-memory node doesn't support eth node; removing this line will cause an error
-      zksync: true,
+      zksync: true
     },
     hardhat: {
-      zksync: true,
-    },
+      zksync: true
+    }
   },
   zksolc: {
     version: "latest",
     settings: {
       // find all available options in the official documentation
       // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
-    },
+    }
   },
   solidity: {
-    version: "0.8.23",
-  },
-};
+    version: "0.8.23"
+  }
+}
 
-export default config;
+export default config
--- semaphorev4/test/greeter.test.ts
+++ semaphorev4/test/greeter.test.ts
@@ -1,21 +1,24 @@
-import { expect } from 'chai';
-import { getWallet, deployContract, LOCAL_RICH_WALLETS } from '../deploy/utils';
+import { expect } from "chai"
+import { getWallet, deployContract, LOCAL_RICH_WALLETS } from "../deploy/utils"
 
-describe('Greeter', function () {
+describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
+    const wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey)
 
-    const greeting = "Hello world!";
-    const greeter = await deployContract("Greeter", [greeting], { wallet, silent: true });
+    const greeting = "Hello world!"
+    const greeter = await deployContract("Greeter", [greeting], {
+      wallet,
+      silent: true
+    })
 
-    expect(await greeter.greet()).to.eq(greeting);
+    expect(await greeter.greet()).to.eq(greeting)
 
-    const newGreeting = "Hola, mundo!";
-    const setGreetingTx = await greeter.setGreeting(newGreeting);
-    
+    const newGreeting = "Hola, mundo!"
+    const setGreetingTx = await greeter.setGreeting(newGreeting)
+
     // wait until the transaction is processed
-    await setGreetingTx.wait();
+    await setGreetingTx.wait()
 
-    expect(await greeter.greet()).to.equal(newGreeting);
-  });
-});
+    expect(await greeter.greet()).to.equal(newGreeting)
+  })
+})
